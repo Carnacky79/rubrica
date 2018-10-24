8f174fc1d458e37c9bded8d5827f8b1554c11369
Francesco Lombardi <francesco.lombardi@pianetasoft.com>
2018-10-25 00:50:03 +0200

    Aggiunta ListView e codice per la visualizzazione dati

--- Rubrica/Form1.Designer.cs
+++ Rubrica/Form1.Designer.cs
@@ -29,12 +29,105 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.listView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dataSet1 = new Rubrica.DataSet1();
+            this.nominativiBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nominativiTableAdapter = new Rubrica.DataSet1TableAdapters.nominativiTableAdapter();
+            this.tableAdapterManager = new Rubrica.DataSet1TableAdapters.TableAdapterManager();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nominativiBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Alignment = System.Windows.Forms.ListViewAlignment.SnapToGrid;
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView.Location = new System.Drawing.Point(13, 13);
+            this.listView.MultiSelect = false;
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(783, 398);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nome";
+            this.columnHeader2.Width = 200;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Cognome";
+            this.columnHeader3.Width = 200;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Telefono";
+            this.columnHeader4.Width = 200;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // nominativiBindingSource
+            // 
+            this.nominativiBindingSource.DataMember = "nominativi";
+            this.nominativiBindingSource.DataSource = this.dataSet1;
+            // 
+            // nominativiTableAdapter
+            // 
+            this.nominativiTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.nominativiTableAdapter = this.nominativiTableAdapter;
+            this.tableAdapterManager.UpdateOrder = Rubrica.DataSet1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(808, 623);
+            this.Controls.Add(this.listView);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nominativiBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource nominativiBindingSource;
+        private DataSet1TableAdapters.nominativiTableAdapter nominativiTableAdapter;
+        private DataSet1TableAdapters.TableAdapterManager tableAdapterManager;
     }
 }
 
--- Rubrica/Form1.cs
+++ Rubrica/Form1.cs
@@ -16,5 +16,51 @@ namespace Rubrica
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: questa riga di codice carica i dati nella tabella 'dataSet1.nominativi'. È possibile spostarla o rimuoverla se necessario.
+            this.nominativiTableAdapter.Fill(this.dataSet1.nominativi);
+
+            LoadList();
+
+
+        }
+
+        private void btnSalva_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.nominativiBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dataSet1);
+
+        }
+
+        private void LoadList()
+        {
+            // Get the table from the data set
+            DataTable dtable = dataSet1.Tables["nominativi"];
+
+            // Clear the ListView control
+            listView.Items.Clear();
+
+            // Display items in the ListView control
+            for (int i = 0; i < dtable.Rows.Count; i++)
+            {
+                DataRow drow = dtable.Rows[i];
+
+                // Only row that have not been deleted
+                if (drow.RowState != DataRowState.Deleted)
+                {
+                    // Define the list items
+                    ListViewItem lvi = new ListViewItem(drow["ID"].ToString());
+                    lvi.SubItems.Add(drow["nome"].ToString());
+                    lvi.SubItems.Add(drow["cognome"].ToString());
+                    lvi.SubItems.Add(drow["tel"].ToString());
+
+                    // Add the list items to the ListView
+                    listView.Items.Add(lvi);
+                }
+            }
+        }
     }
 }
