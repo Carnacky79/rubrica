3aa0dc429b31a5710e9b1d7449da8e25a22a30c5
Francesco Lombardi <francesco.lombardi@pianetasoft.com>
2018-10-28 04:40:42 +0100

    Aggiunta casella ricerca - TODO: funzionalità

--- Rubrica/Form1.Designer.cs
+++ Rubrica/Form1.Designer.cs
@@ -38,8 +38,17 @@
             this.nominativiBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.nominativiTableAdapter = new Rubrica.DataSet1TableAdapters.nominativiTableAdapter();
             this.tableAdapterManager = new Rubrica.DataSet1TableAdapters.TableAdapterManager();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.srcTxtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.srcTxtCognome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.srcTxtTelefono = new System.Windows.Forms.TextBox();
+            this.scrBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nominativiBindingSource)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // listView
@@ -102,17 +111,93 @@
             this.tableAdapterManager.nominativiTableAdapter = this.nominativiTableAdapter;
             this.tableAdapterManager.UpdateOrder = Rubrica.DataSet1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.scrBtn);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.srcTxtTelefono);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.srcTxtCognome);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.srcTxtNome);
+            this.groupBox1.Location = new System.Drawing.Point(13, 417);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(196, 138);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ricerca";
+            // 
+            // srcTxtNome
+            // 
+            this.srcTxtNome.Location = new System.Drawing.Point(77, 19);
+            this.srcTxtNome.Name = "srcTxtNome";
+            this.srcTxtNome.Size = new System.Drawing.Size(113, 20);
+            this.srcTxtNome.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cognome:";
+            // 
+            // srcTxtCognome
+            // 
+            this.srcTxtCognome.Location = new System.Drawing.Point(77, 49);
+            this.srcTxtCognome.Name = "srcTxtCognome";
+            this.srcTxtCognome.Size = new System.Drawing.Size(113, 20);
+            this.srcTxtCognome.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Telefono:";
+            // 
+            // srcTxtTelefono
+            // 
+            this.srcTxtTelefono.Location = new System.Drawing.Point(77, 79);
+            this.srcTxtTelefono.Name = "srcTxtTelefono";
+            this.srcTxtTelefono.Size = new System.Drawing.Size(113, 20);
+            this.srcTxtTelefono.TabIndex = 4;
+            // 
+            // scrBtn
+            // 
+            this.scrBtn.Location = new System.Drawing.Point(77, 107);
+            this.scrBtn.Name = "scrBtn";
+            this.scrBtn.Size = new System.Drawing.Size(113, 23);
+            this.scrBtn.TabIndex = 6;
+            this.scrBtn.Text = "Cerca";
+            this.scrBtn.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(808, 623);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.listView);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nominativiBindingSource)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -128,6 +213,14 @@
         private System.Windows.Forms.BindingSource nominativiBindingSource;
         private DataSet1TableAdapters.nominativiTableAdapter nominativiTableAdapter;
         private DataSet1TableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button scrBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox srcTxtTelefono;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox srcTxtCognome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox srcTxtNome;
     }
 }
 
