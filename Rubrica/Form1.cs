d1d850871091d099a3de09721819dd212926c985
Francesco Lombardi <francesco.lombardi@pianetasoft.com>
2018-10-31 02:24:15 +0100

    Release 1.0.0

--- Rubrica/Form1.cs
+++ Rubrica/Form1.cs
@@ -37,6 +37,12 @@ namespace Rubrica
         }
 
 
+        private void loadComboBox()
+        {
+            char[] letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+        }
+
         private void LoadList()
         {
             // Get the table from the data set
